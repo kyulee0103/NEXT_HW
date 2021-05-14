75f679ca251b400a5b0a6235fae8dde7b6c59d35
KyuleeKwon <kyulee1338@gmail.com>
2021-05-14 19:01:41 +0900

    new commit

---
+++ Session8-1_HW/project/app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    deadline = models.DateTimeField(null= True, blank = True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
